aac0df6ffe47a5815169164f40248a354797dce1
李政林 <182336408@qq.com>
2025-09-12 12:44:13 +0800

    删除多余页面

--- law-ui/src/layout/components/Navbar.vue
+++ law-ui/src/layout/components/Navbar.vue
@@ -9,13 +9,6 @@
       <template v-if="device!=='mobile'">
         <search id="header-search" class="right-menu-item" />
 
-        <el-tooltip content="源码地址" effect="dark" placement="bottom">
-          <law-office-git id="law-office-git" class="right-menu-item hover-effect" />
-        </el-tooltip>
-
-        <el-tooltip content="文档地址" effect="dark" placement="bottom">
-          <law-office-doc id="law-office-doc" class="right-menu-item hover-effect" />
-        </el-tooltip>
 
         <screenfull id="screenfull" class="right-menu-item hover-effect" />
 
@@ -54,8 +47,6 @@ import Hamburger from '@/components/Hamburger'
 import Screenfull from '@/components/Screenfull'
 import SizeSelect from '@/components/SizeSelect'
 import Search from '@/components/HeaderSearch'
-import LawOfficeGit from '@/components/LawOffice/Git'
-import LawOfficeDoc from '@/components/LawOffice/Doc'
 
 export default {
   emits: ['setLayout'],
@@ -65,9 +56,7 @@ export default {
     Hamburger,
     Screenfull,
     SizeSelect,
-    Search,
-    LawOfficeGit,
-    LawOfficeDoc
+    Search
   },
   computed: {
     ...mapGetters([
